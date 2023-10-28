eaf0b87d097b88cf94f3f2f0438b829e5742a7d1
balaji <balaji.kukati341@gmail.com>
2023-10-28 16:38:44 +0530

    updated shell

--- shell.sh
+++ shell.sh
@@ -1,2 +1,10 @@
+<<<<<<< HEAD
 iam going to apply 
 hii shell script .......hoooo
+=======
+third time update
+hello this is shell script
+iam going to apply 
+hii shell script .......hoooo
+
+>>>>>>> 3a00260 (update all files)
