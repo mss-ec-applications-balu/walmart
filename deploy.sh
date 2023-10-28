747016437704f0ac2d0ed2475b6194eb7e99d9d6
balaji <balaji.kukati341@gmail.com>
2023-10-28 17:01:57 +0530

    Revert "update all files"
    This reverts commit 3a00260265ead61fe23febb7c99ce72f4caaf769.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-third time update
 Hi iam from master 
 hello upadating is over
 this file contains deployment script
--- shell.sh
+++ shell.sh
@@ -1,5 +1,6 @@
-third time update
+<<<<<<< HEAD
 hello this is shell script
+=======
 iam going to apply 
 hii shell script .......hoooo
-
+>>>>>>> 9b8c448 (update shell)
